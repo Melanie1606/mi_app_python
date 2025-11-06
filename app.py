15d61f99dfaf234647b755b78716572bbdf7f346
Melanie1606 <msc.atancuri@yavirac.edu.ec>
2025-11-06 11:18:40 -0500

    Añadir CI/CD Melanie y contenedor melanie:1.0.0

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hola desde mi app Python en GitHub!") 
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hola, soy Melanie y este es mi contenedor corriendo correctamente 🎉"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
